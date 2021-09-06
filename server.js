a3c8779e57109d5419a6e7ace9d1ce4402431d62
whoward2929 <Whoward939@gmail.com>
2021-09-05 23:48:44 -0400

    server changes

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const express = require('express');
-const HTMLroutes = require('./routes/html.js');
-const apiRoutes = require('./routes/api.js');
-
+const path = require('path')
+const fs = require('fs');
+const noteArray = require('./db/db')
+const uniqid = require('uniqid')
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', HTMLroutes);
-app.use('/api', apiRoutes);
-app.listen(PORT, () =>{
-    console.log(`API server now on ${PORT}!`);
+app.use(express.static('public'));
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+app.get('/api/notes', (req, res) => {
+    return res.json(noteArray);
+})
+app.post('/api/notes', (req, res) => {
+    let note = req.body;
+    note.id = uniqid();
+    noteArray.push(note);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray), null, 2);
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+app.delete('/api/notes/:id', (req, res) => {
+    const deleteNote = req.params.id;
+    noteArray.forEach((element, index) => {
+        if(element.id === deleteNote){
+            noteArray.splice(index,1);
+        }
+    });
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray), null, 2);
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
